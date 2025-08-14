074bce5ed6b6bc8f1297f9fb3ee7ef2376d9c6ee
nimyathomas <nimyathomas2025@mca.ajce.in>
2025-08-14 12:32:46 +0530

    Move app files to root for deployment

--- project/models.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class URL(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    long_url = db.Column(db.String(500), nullable=False)
-    short_code = db.Column(db.String(10), unique=True, nullable=False)
-    clicks = db.Column(db.Integer, default=0)  # ensure this line exists
