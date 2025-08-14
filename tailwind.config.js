468a9045267b0d4ff2d8c1fae013e2435d8312d4
nimyathomas <nimyathomas2025@mca.ajce.in>
2025-08-14 12:11:24 +0530

    Prepare app for Railway deployment

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: 'class', // enable class-based dark mode
+  content: [
+    "./templates/**/*.html",
+    "./static/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
